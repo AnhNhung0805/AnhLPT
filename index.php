ae38a5dcd7212957c8d20d9783e2c5cc905d64f4
Tuấn Anh Lê Phan <anhlptgcd18651@fpt.edu.vn>
2021-03-14 20:15:20 +0700

    anhlptgcd18651

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+</head>
+
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>  
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo">
+                <a href="https://www.facebook.com/lephan.tuananh.90/">Tuan<span>Anh ^^</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">-Home-</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">My Hobbies</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">
+                    Hello, my name is</div>
+                <div class="text-2">Le Phan Tuan Anh</div>
+                <div class="text-3">And I'm a <span class="typing">Intern Fsoft</span></div>
+                <a href="Demo/index.html">Details for video introduce</a>
+            </div>
+        </div>
+    </section>
+    <script src="script.js"></script>
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">
+                About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="Anha.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">
+                        Hello Everybody <span class="typing-2"></span></div>
+                    <a href="Anhlpt.pdf">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">
+                My Hobbies Is</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">
+                            Design and Play Game</div>
+                        <p>
+                            Paint, Edit and Effect Video, Design Mind Map. Sometime I play game such as: LOL, Contra,...</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">
+                            Travel</div>
+                        <p>
+                            I went to beautiful places by motorbike, would you like to see it ?  </p>
+                        
+                        <a href="pagetravel/pagetravel.html">Details</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">
+                            Code For Fun</div>
+                        <p>
+                            I readly readly like code, but i will fell so sad if my code has many bugs =v. Easy to fix bug if i try try try try... </p>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">
+                My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">
+                        Experienes and skills</div>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span></span>
+                        </div>
+                        <div class="line html">
+                        </div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span></span>
+                        </div>
+                        <div class="line css">
+                        </div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span></span>
+                        </div>
+                        <div class="line js">
+                        </div>
+                    </div>
+                    <!-- <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span></span>
+                        </div>
+                        <div class="line php">
+                        </div>
+                    </div> -->
+                    <div class="bars">
+                        <div class="info">
+                            <span>SQL Server Basic</span>
+                            <span></span>
+                        </div>
+                        <div class="line mysql">
+                        </div>
+                    </div>
+                    <!-- <div class="bars">
+                        <div class="info">
+                            <span>Python</span>
+                            <span></span>
+                        </div>
+                        <div class="line python">
+                        </div>
+                    </div> -->
+                    <!-- <div class="bars">
+                        <div class="info">
+                            <span>C#</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line magento">
+                        </div>
+                    </div> -->
+                    <!-- <div class="bars">
+                        <div class="info">
+                            <span>Laravel</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line laravel">
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">
+                My teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="Anh.jpg" alt="">
+                        <div class="text">
+                            Le Phan Tuan Anh</div>
+                        <a href="http://www.facebook.com">
+                            View</a>
+                    </div>
+                </div>
+                <div class="card" href="facebook.com">
+                    <div class="box">
+                        <img src="Linh.jpg" alt="facebook.com">
+                        <div class="text">
+                            Ho Duc Linh</div>
+                        <a href="http://www.facebook.com">
+                                View</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="thang.jpg" alt="">
+                        <div class="text">
+                            Pham Van Thang</div>
+                        <a href="http://www.facebook.com">
+                                View</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="Vy.jpg" alt="">
+                        <div class="text">
+                            Dang The Vy</div>
+                        <a href="http://www.facebook.com">
+                                View</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="phuong.jpg" alt="">
+                        <div class="text">
+                            Pham Van Phuong</div>
+                        <a href="http://www.facebook.com">
+                                View</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">
+                Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">
+                        Connect with me</div>
+                    <p>
+                        If you want to need me or do work, please flow imformation here</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">
+                                    Name</div>
+                                <div class="sub-title">
+                                    Le Phan Tuan Anh</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">
+                                    Address</div>
+                                <div class="sub-title">
+                                    Da Nang</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">
+                                    Email</div>
+                                <div class="sub-title">
+                                    anhroyal110@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">
+                        Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <!-- Due to more textarea, I got an error so I changed the tag name of this textarea into changeit. -->
+                            <changeit cols="30" rows="10" placeholder="Message.." required></changeit>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.facebook.com/lephan.tuananh.90/">Tuan Anh</a> | <i class="fas fa-spinner fa-spin" style="font-size:24px;"></i> run time 2021 .</span>
+    </footer>
+
+</body>
+
+</html>
